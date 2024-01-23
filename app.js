668a96b7f02867a51ea0ecb6201b6de23b5f6e6d
shivamkathyala <shivamkathyala@gmail.com>
2024-01-23 18:25:23 +0530

    In this change I added a draw game logic

--- app.js
+++ app.js
@@ -3,13 +3,17 @@ let resetBtn = document.querySelector("#reset");
 let newGameBtn = document.querySelector("#new-game");
 let msgContainer = document.querySelector(".winnerInfo");
 let msg = document.querySelector(".msg");
+let gameContainer = document.querySelector(".container");
 
 let turnO = true;  //player x, player o
-
+let winner = false; //to track draw
 const resetGame = ()=>{
     turnO = true;
+    winner = false;
     enableBoxes();
     msgContainer.classList.add("hide");
+    gameContainer.classList.remove("rainbow");
+
 };
 const winPattern = [
     [0,1,2],
@@ -21,11 +25,12 @@ const winPattern = [
     [3,4,5],
     [6,7,8]
 ];
-
+let clickCount= 0;
 boxes.forEach((box) => {
 box.addEventListener("click", ()=>{
-    //console.log("Box was clicked");
-    box.innerText = "abcd";
+    gameContainer.classList.add("rainbow");
+    clickCount++;
+    console.log(clickCount);
     if(turnO){ //player O
         box.innerText = "O";
         turnO = false;
@@ -35,6 +40,11 @@ box.addEventListener("click", ()=>{
     }
     box.disabled = true;
     checkWinner();
+    if( clickCount === 9 && winner === false){
+        console.log("Draw");
+        showDraw();
+    }
+
 })
     
 });
@@ -49,6 +59,7 @@ const enableBoxes = ()=>{
     for(let box of boxes){
         box.disabled = false;
         box.innerText = "";
+        clickCount= 0;
     }
 };
 
@@ -57,11 +68,13 @@ const showWinner = (winner)=>{
     msgContainer.classList.remove("hide");
     disableBoxes();
 };
+const showDraw = ()=>{
+    msg.innerText = "The Game is Draw";
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+}
 const checkWinner = ()=>{
     for(let pattern of winPattern){
-        //console.log(pattern[0], pattern[1], pattern[2]);
-        //console.log(boxes[pattern[0]].innerText, boxes[pattern[1]].innerText, boxes[pattern[2]].innerText);
-
         let pos1Val = boxes[pattern[0]].innerText;
         let pos2Val = boxes[pattern[1]].innerText;
         let pos3Val = boxes[pattern[2]].innerText;
@@ -70,6 +83,7 @@ const checkWinner = ()=>{
            if( pos1Val === pos2Val && pos2Val === pos3Val){
             console.log("Winner", pos1Val);
             showWinner(pos1Val);
+            winner = true;
            } 
         }
     }
